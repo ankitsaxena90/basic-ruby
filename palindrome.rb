1cf994535ff8f3fbf9527489b9c60fcd9bdd4197
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-05 04:26:05 +0530

    palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,6 @@
 @regex = /^[qQ]$/
 def check_palindrome(input_string)
-	if input_string.reverse == input_string
+  if input_string.reverse == input_string
     puts "#{ input_string } is a palindrome."
   else
     puts "#{ input_string } is not a palindrome."
@@ -8,11 +8,11 @@ def check_palindrome(input_string)
 end
 
 while true
-	puts "Enter any string (q/Q to exit)"
+  puts "Enter any string (q/Q to exit)"
   input_string = gets.chomp.downcase
   if input_string.match(@regex)
-	  break
-	else
-	check_palindrome(input_string)
+    break
+  else
+    check_palindrome(input_string)
   end
 end
