0c20d1ff9d238b1d3c909a6cbc63ac1572567738
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-05 03:16:26 +0530

    replace vowel

--- replace_vowel.rb
+++ replace_vowel.rb
@@ -1,8 +1,7 @@
 def replaceVowel(input_string)
-  #puts vowel_string
   vowels = /[aeiou]/i
   puts input_string.gsub(vowels,"*")
 end
 puts "Enter any string"
-input_string = gets.chomp.downcase
+input_string = gets.chomp
 replaceVowel(input_string)
