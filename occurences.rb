c02a7f03da00549ab594b34930f536076a31d4be
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-05 01:50:45 +0530

    count characters in string

--- occurences.rb
+++ occurences.rb
@@ -1,15 +1,15 @@
 @occurences = Hash.new(0)
 def characterCount(input_string)
-	input_string.each_char do |character|
-		if character != " " 
-			@occurences[character] += 1
-		end
-	end
+  input_string.each_char do |character|
+    if character != " " 
+      @occurences[character] += 1
+    end
+  end
 end
 
 puts "Enter the input String"
 input_string = gets.chomp.downcase
 characterCount(input_string)
 @occurences.each do |key, value|
-	puts "#{key} occured #{value} times"
+  puts "#{key} occured #{value} times"
 end
