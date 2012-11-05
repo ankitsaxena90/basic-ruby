9924ddb3842c3ff6ebe707ab7c41a9d04e2e1323
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-05 16:35:11 +0530

    array hash exercise

--- array-hash.rb
+++ array-hash.rb
@@ -1,8 +1,8 @@
 @occurences = Hash.new(Array.new())
 def power(input_array)
-	for i in input_array
-			@occurences[i.to_s.length] += [i]
-	end
+  for i in input_array
+    @occurences[i.to_s.length] += [i]
+  end
 end
 
 input_array = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'] 
