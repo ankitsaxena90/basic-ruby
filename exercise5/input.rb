478233f9a7581076bbc433c8062265fbc85cc91f
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-05 11:07:57 +0530

    customer exercise5

--- exercise5/input.rb
+++ exercise5/input.rb
@@ -5,7 +5,7 @@ customer1.deposit(2000)
 customer1.withdraw(1500)
 customer1.displayDetails()
 
-customer2 = Customer.new('jsnkn')
+customer2 = Customer.new('John')
 customer2.deposit(4000)
 customer2.withdraw(2500)
 customer2.displayDetails()
