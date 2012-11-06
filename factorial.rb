e5b26589155b20b41afe359def4d4b0ddb33083b
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-06 11:38:06 +0530

    Factorial

--- factorial.rb
+++ factorial.rb
@@ -1,13 +1,13 @@
 def factorial(number)
-	(1..number).inject { |fact, var| fact * var }
+  (1..number).inject { |fact, var| fact * var }
 end
 
 puts "Enter any number"
 input_number = gets.chomp
 digit_pattern = /^\d+$/
 if input_number.match(digit_pattern)
-	result = factorial(input_number.to_i)
-	puts "Factorial of #{input_number} is #{result}"
+  result = factorial(input_number.to_i)
+  puts "Factorial of #{input_number} is #{result}"
 else
-	puts "Invalid input enetered"
+  puts "Invalid input enetered"
 end
