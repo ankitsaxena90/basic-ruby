65ab3c6f664c3e9084bff175755927332c5faefd
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-07 01:55:46 +0530

    hash-inject

--- hash-inject/hash_new.rb
+++ hash-inject/hash_new.rb
@@ -1,13 +1,13 @@
 require_relative './array-hash'
 
 def generateHash(input_array)
-	hash = createHash(input_array)
-	container_hash = {}
+  hash = createHash(input_array)
+  container_hash = {}
   container_hash['even'] = []
   container_hash['odd'] = []
   group_hash = hash.inject(container_hash) do |container_hash, element|
-    if element[0].to_i.even?
-      container_hash['even'] << element[1]
+  if element[0].to_i.even?
+    container_hash['even'] << element[1]
     else
       container_hash['odd'] << element[1]
     end
