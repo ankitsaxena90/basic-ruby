c7440c598c9ca604c188f9f1053c71dbf7386a56
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-07 13:21:51 +0530

    name exception

---
+++ name_exception.rb
@@ -0,0 +1,11 @@
+class Name
+	attr_accessor:firstname, :lastname
+	def initialize(firstname, lastname)
+		raise "Null Value entered" if firstname.length == 0 || lastname.length == 0
+		raise "First letter of first name is not Capital" unless (firstname[0] =~ /[A-Z]/) != nil
+		puts @firstname = firstname
+		puts @lastname = lastname
+	end
+end
+
+name_obj = Name.new("Ankit","Saxena")
