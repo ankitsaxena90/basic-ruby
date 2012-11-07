58e46786843a02ecdcb2f383f7e6b2320cc96c2e
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-07 16:47:04 +0530

    reading/writing csv data

--- csv/read_data.rb
+++ csv/read_data.rb
@@ -2,18 +2,13 @@ emp_hash =  Hash.new(Array.new)
 read_file = File.open("employee_data.csv", "r")
 read_file.each_line { |line| 
 	fields = line.split(",")
-	puts "#{fields[0]} #{fields[1]} #{fields[2]}"
+	#puts "#{fields[0]} #{fields[1]} #{fields[2]}"
 	
 	name = fields[0]
 	emp_id = fields[1]
 	role = fields[2]
 	emp_hash[role] += ["#{name} (EmpId: #{emp_id})"]
 }
-emp_hash.each_pair { |name, val|  
-	puts name
-	puts val
-}
-
 write_file = File.open("output.csv","w")
 emp_hash.each_pair { |name, val| 
 	write_file.puts name 
