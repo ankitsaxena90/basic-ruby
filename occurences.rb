2473b11ccbe42b26a7aa97871481dc51eb4649fa
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-16 10:53:24 +0530

    updated file

--- occurences.rb
+++ occurences.rb
@@ -1,7 +1,8 @@
-@occurences = Hash.new(0)
-def characterCount(input_string)
+def character_count(input_string)
+  @occurences = Hash.new(0)
   input_string.each_char do |character|
-    if character != " " 
+    @regex = /([a-zA-Z])/
+    if character.match(@regex)
       @occurences[character] += 1
     end
   end
@@ -9,7 +10,7 @@ end
 
 puts "Enter the input String"
 input_string = gets.chomp.downcase
-characterCount(input_string)
+character_count(input_string)
 @occurences.each do |key, value|
   puts "#{key} occured #{value} times"
 end
