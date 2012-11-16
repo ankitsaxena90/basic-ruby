76b07468f4a6dcf00d6e7626ced33e05a7bab023
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-16 11:01:54 +0530

    updated file

--- occurences.rb
+++ occurences.rb
@@ -1,8 +1,8 @@
 def character_count(input_string)
   @occurences = Hash.new(0)
   input_string.each_char do |character|
-    @regex = /([a-zA-Z])/
-    if character.match(@regex)
+    regex = /([a-zA-Z])/
+    if character.match(regex)
       @occurences[character] += 1
     end
   end
