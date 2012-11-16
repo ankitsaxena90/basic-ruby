a04902a8d25f3ce15572819c12b391465d7d4c73
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-16 14:55:44 +0530

    updated factorial exercise

--- factorial.rb
+++ factorial.rb
@@ -2,12 +2,17 @@ def factorial(number)
   (1..number).inject { |fact, var| fact * var }
 end
 
+def check_digit(input_number) 
+  digit_pattern = /^\d+$/
+  if input_number.match(digit_pattern)
+    result = factorial(input_number.to_i)
+    puts "Factorial of #{input_number} is #{result}"
+  else
+    puts "Invalid input enetered"
+  end
+end
 puts "Enter any number"
 input_number = gets.chomp
-digit_pattern = /^\d+$/
-if input_number.match(digit_pattern)
-  result = factorial(input_number.to_i)
-  puts "Factorial of #{input_number} is #{result}"
-else
-  puts "Invalid input enetered"
-end
+check_digit(input_number)
+
+
