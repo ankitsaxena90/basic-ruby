a77579a234dd9a56e61022320f0a09cfaab35de4
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-16 11:47:32 +0530

    updated code

--- power.rb
+++ power.rb
@@ -1,11 +1,11 @@
-$result_array = []
 class Array
   def power!(exponent)
-    self.each {|n| $result_array.push(n ** exponent) }
-    $result_array
+    result_array = []
+    self.each {|n| result_array.push(n ** exponent) }
+    result_array
   end
 end
 
 array1 = [1, 2, 3, 4]
-array1.power!(3)
-puts $result_array.inspect
+puts array1.power!(3)
+
