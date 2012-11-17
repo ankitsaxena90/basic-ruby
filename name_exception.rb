5f09d821bfd747ff3a48d7ce8abbd80af96d9de8
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-17 13:40:08 +0530

    name exception

--- name_exception.rb
+++ name_exception.rb
@@ -1,11 +1,11 @@
 class Name
-	attr_accessor:firstname, :lastname
 	def initialize(firstname, lastname)
 		raise "Null Value entered" if firstname.length == 0 || lastname.length == 0
 		raise "First letter of first name is not Capital" unless (firstname[0] =~ /[A-Z]/) != nil
-		puts @firstname = firstname
+		print @firstname = firstname, " "
 		puts @lastname = lastname
 	end
 end
 
-name_obj = Name.new("","")
+name_obj1 = Name.new("john","Thomas")
+name_obj2 = Name.new("","")
