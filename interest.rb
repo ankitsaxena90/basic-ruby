3ca0bd43218a452dacf673071cd4222adbed608e
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-17 13:35:15 +0530

    interest

--- interest.rb
+++ interest.rb
@@ -1,20 +1,19 @@
 class Interest
 	@@rate = 50.0
-	attr_accessor:principal, :time
 	def initialize (principal, time)
 		@principal = principal
 		@time = time
 	end
 
-	def simpleInterest
+	def simple_interest
 		return @principal + (@principal * @@rate * @time) / 100
 	end
 
-	def compoundInterest
+	def compound_interest
 		return @principal * (1 + @@rate / 100) ** @time
 	end
 end
 
 interest1 = Interest.new(5000, 2)
-amount_difference = interest1.compoundInterest - interest1.simpleInterest
+amount_difference = interest1.compound_interest - interest1.simple_interest
 puts "Difference in amount calculated compoundedly and simply is #{amount_difference}"
