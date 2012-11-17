19638b2b905917ed392e52025b2e5a614fb341e9
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-17 13:22:15 +0530

    updated code

--- exercise6/vehicle.rb
+++ exercise6/vehicle.rb
@@ -9,14 +9,3 @@ class Vehicle
   end
 end
 
-class Bike < Vehicle
-  attr_accessor:price
-  def initialize(name, price, dealer)
-    super(name, price)
-    @dealer = dealer
-  end
-
-  def display_details()
-    puts "Bike is #{@name}  Price: Rs. #{@price}  Dealer:#{@dealer}"
-  end
-end
