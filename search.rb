54cfb8b1b140419240879ba5c9cb1d98aed0f8a4
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-19 15:43:06 +0530

    using gsub block

--- search.rb
+++ search.rb
@@ -1,15 +1,11 @@
-$count = 0
 def search(input_string, input_word)
-	search_pattern = /#{input_word}/i
-	string =  input_string.split(" ")
-	arr_string = string.to_a
-	puts input_string.gsub(search_pattern, "(#{input_word})")
-	string.each do |str| 
-		if str.match(search_pattern)
-			$count += 1
-		end
-	end
-	puts "Total Occurences found: #{$count}"
+  search_pattern = /#{input_word}/i
+  count = 0
+  puts input_string.gsub(/#{input_word}/i) {|match| 
+    count += 1
+    "(#{match})"
+  }
+  puts "Total Occurences found: #{count}"
 end
 
 puts "Enter any string"
