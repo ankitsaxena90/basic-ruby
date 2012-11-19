f00fce88542042b8dbcedc0b034645508f1e26c6
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-19 11:39:56 +0530

    updated code

--- exercise6/bike.rb
+++ exercise6/bike.rb
@@ -1,14 +1,12 @@
 require_relative './vehicle'
-
 class Bike < Vehicle
-  attr_accessor:price
   def initialize(name, price, dealer)
     super(name, price)
     @dealer = dealer
   end
 
   def display_details()
-    puts "Bike is #{@name}  Price: Rs. #{@price}  Dealer:#{@dealer}"
+    super()
+    puts "Dealer:#{@dealer}"
   end
 end
-
--- exercise6/vehicle.rb
+++ exercise6/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle
   attr_accessor:name, :price
   def initialize(name, price)
     @name = name
-    @price = 60000
+    @price = price
   end
   def display_details()
     puts "Bike is #{@name}  Price: Rs. #{@price}"
