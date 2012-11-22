af92249798892d831f9f6b6e70b430055a117b3b
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-22 19:00:53 +0530

    updated code

--- area.rb
+++ area.rb
@@ -1,20 +1,21 @@
 def area(*args)
-  case args[0]
-  when "circle"
-    area =  3.14 * args[1] * args[1]
+  case args.length
+  when 1
+    area =  3.14 * args[0] * args[0]
     puts "Area of Circle is #{area}"
 
-  when "rectangle"
-    area = args[1] * args[2]
+  when 2
+    area = args[0] * args[1]
     puts "Area of Rectangle is #{area}"
 
-  when "triangle"
-    area = 0.5 * args[1] * args[2]
+  when 3
+    s = args[0] + args[1] + args[2] / 2.0
+    area = Math.sqrt(s * (s - args[0]) * (s - args[1]) * (s-args[2])).round(2)
     puts "Area of triangle is #{area}"
   end
 end
 
 
-area("circle", 2)
-area("rectangle", 2,3)
-area("triangle", 4,5)
+area(2)
+area(2,3)
+area(3,4,5)
