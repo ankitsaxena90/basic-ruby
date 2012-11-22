327c5a209ff2a6ca975a20235a06eae99192eb39
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-22 17:25:04 +0530

    improved indentation

--- csv/read_data.rb
+++ csv/read_data.rb
@@ -1,16 +1,16 @@
 emp_hash =  Hash.new(Array.new)
 read_file = File.open("employee_data.csv", "r")
 read_file.each_line { |line| 
-	fields = line.split(",")
-	#puts "#{fields[0]} #{fields[1]} #{fields[2]}"
-	
-	name = fields[0]
-	emp_id = fields[1]
-	role = fields[2]
-	emp_hash[role] += ["#{name} (EmpId: #{emp_id})"]
+  fields = line.split(",")
+  #puts "#{fields[0]} #{fields[1]} #{fields[2]}"
+  
+  name = fields[0]
+  emp_id = fields[1]
+  role = fields[2]
+  emp_hash[role] += ["#{name} (EmpId: #{emp_id})"]
 }
 write_file = File.open("output.csv","w")
 emp_hash.each_pair { |name, val| 
-	write_file.puts name 
-	write_file.puts val
+  write_file.puts name 
+  write_file.puts val
 }
