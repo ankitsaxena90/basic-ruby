66d25ccfdc3cd7bb3b13dae831d825f76e35a132
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-22 18:07:43 +0530

    factorial exception updated code

--- factorial_exception.rb
+++ factorial_exception.rb
@@ -1,10 +1,10 @@
 def factorial(num)
-  raise ArgumentError, 'Argument is negative' if num < 0
+  raise ArgumentError, 'Try with a positive value'  if num < 0
   return 1 if num == 0
   num * factorial(num - 1)
 end
 
 begin puts factorial(-5)
 rescue ArgumentError => ex
-  puts "Try again with a positive value"
+  puts ex
 end
