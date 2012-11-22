a762e8da2d891c1703f6bcef3e7d1d5c752aa702
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-22 17:29:58 +0530

    overloading ex- improved indentation

--- area.rb
+++ area.rb
@@ -1,17 +1,17 @@
 def area(*args)
-	case args[0]
-	when "circle"
-		area =  3.14 * args[1] * args[1]
-		puts "Area of Circle is #{area}"
+  case args[0]
+  when "circle"
+    area =  3.14 * args[1] * args[1]
+    puts "Area of Circle is #{area}"
 
-	when "rectangle"
-		area = args[1] * args[2]
-		puts "Area of Rectangle is #{area}"
+  when "rectangle"
+    area = args[1] * args[2]
+    puts "Area of Rectangle is #{area}"
 
-	when "triangle"
-		area = 0.5 * args[1] * args[2]
-		puts "Area of triangle is #{area}"
-	end
+  when "triangle"
+    area = 0.5 * args[1] * args[2]
+    puts "Area of triangle is #{area}"
+  end
 end
 
 
