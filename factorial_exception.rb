3a4920dea52fdf08b6f42dca536c5bec297a67d5
ankitsaxena90 <ankit.saxena@vinsol.com>
2012-11-22 17:26:47 +0530

    factorial- improved indentation

--- factorial_exception.rb
+++ factorial_exception.rb
@@ -1,10 +1,10 @@
 def factorial(num)
-	raise ArgumentError, 'Argument is negative' if num < 0
-	return 1 if num == 1
-	num * factorial(num - 1)
+  raise ArgumentError, 'Argument is negative' if num < 0
+  return 1 if num == 1
+  num * factorial(num - 1)
 end
 
 begin puts factorial(-5)
 rescue ArgumentError => ex
-	puts "Try again with a positive value"
+  puts "Try again with a positive value"
 end
